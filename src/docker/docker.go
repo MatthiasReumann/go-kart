e2c4924a5be1dad87bba666799def1543ea074c4
Matthias Reumann <matthias.reumann@tum.de>
2022-05-15 14:33:10 +0200

    Add DockerClient struct

--- src/docker/docker.go
+++ src/docker/docker.go
@@ -1,1 +1,49 @@
 package docker
+
+import (
+	"context"
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
+	"os"
+)
+
+type DockerClient interface {
+	Build(string) error
+	Run()
+	Remove()
+	RemoveImage()
+}
+
+type dockerClient struct {
+	cli *client.Client
+}
+
+func NewDockerClient() (DockerClient, error) {
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		return nil, err
+	}
+	return dockerClient{cli}, nil
+}
+
+func (c dockerClient) Build(dockerfile string) error {
+	ctx := context.Background()
+	f, err := os.Open(dockerfile)
+	if err != nil {
+		return err
+	}
+	_, err = c.cli.ImageBuild(ctx, f, types.ImageBuildOptions{})
+	return err
+}
+
+func (c dockerClient) Run() {
+
+}
+
+func (c dockerClient) Remove() {
+
+}
+
+func (c dockerClient) RemoveImage() {
+
+}
