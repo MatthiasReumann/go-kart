234a867f2ec0b075dba995e0f2afff295d63150f
Matthias Reumann <matthias.reumann@tum.de>
2022-05-15 14:20:00 +0200

    Update main

--- src/main.go
+++ src/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"github.com/matthiasreumann/go-kart/src/config"
+	"github.com/matthiasreumann/go-kart/src/router"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("hello from go-kart")
+	endpoints := []config.Endpoint{
+		{"/test", "/Volumes/T/work/go-kart/test-files/Dockerfile"},
+	}
+
+	r := router.NewRouter(endpoints)
+	err := http.ListenAndServe(":1337", r)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
